77ed14897439743d412065d77d7e09e36ffadae1
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-04 03:50:57 +0000

    comparing two arg in a function with eqAssert

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+function eqArrays(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([2, 2, 3], [2, 2, 4]);
