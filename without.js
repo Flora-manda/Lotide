c4380974f5c7ed1f5f1e13e135c02d47ad23a8f2
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-04 06:14:49 +0000

    A function with two arrays as args. First array should be outputted without elements from the second array.

---
+++ without.js
@@ -0,0 +1,35 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] === itemsToRemove[i]) {
+      source.splice(itemsToRemove[i] - 1, 1);
+      return source;
+    }
+  }
+  return false;
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
