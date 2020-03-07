275c9accaf2cf3020349d2d7789cb6f664af0302
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-07 02:03:47 +0000

    building a map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const callback = map(words, word => word.toUpperCase());
+
+//--------Test code begins--------------
+function eqArrays(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//-----------Test code ends---------------
+
+assertArraysEqual(callback, ["GROUND", "CONTROL", "TO","MAJOR", "TOM"]);
