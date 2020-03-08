69c776f5a844fc050ab7f19370c47444a286fb5a
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-08 01:33:54 +0000

    take values from an array until a certain point

---
+++ takeUntil.js
@@ -0,0 +1,51 @@
+const takeUntil = function(array, callback) {
+  let final = [];
+  for (let items of array) {
+    final.push(items);
+    if (callback(items)) {
+      final.pop(items);
+      break;
+    }
+  }
+  return final;
+}
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+
+//--------Test code begins--------------
+function eqArrays(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//-----------Test code ends---------------
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
