f844f55598f4b9048d17ab6a328aa7445079128e
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-09 19:02:40 +0000

    refactored code

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 function eqArrays(firstArray, secondArray) {
   if (firstArray.length !== secondArray.length) {
     return false;
@@ -19,6 +11,4 @@ function eqArrays(firstArray, secondArray) {
   }
 }
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), false);
+module.exports = eqArrays;
