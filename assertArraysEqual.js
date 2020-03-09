6b03c545cc69c58053193ad11e4f171c598b6c17
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-09 22:11:18 +0000

    refactored code for Lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,10 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    return `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
   }
 };
 
--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
 function assertEqual(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    return `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
   }
 };
 
