615c97b61937b3142d7039d00d1dad66b606e323
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-09 22:14:04 +0000

    refactored test code for Lotide

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,15 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Hello", "Hello");
-assertEqual(2, 3);
+describe("#assertEqual", () => {
+
+  it("should compare two values that are not arrays and return true", () => {
+    assert.deepEqual(assertEqual(1, 1), `✅✅✅ Assertion Passed: 1 === 1`);
+  })
+
+  it("should compare two values that are not arrays and return false", () => {
+    assert.deepEqual(assertEqual("Hello", "Hi"), `❌❌❌ Assertion Failed: Hello !== Hi`);
+  })
+
+});
+
